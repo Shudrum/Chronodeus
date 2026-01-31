ab6e0e678b3c8f564c81ff8b8f29e108dde869e1
Shudrum <m@rtin.tech>
2026-02-01 00:32:16 +0100

    New camera system

---
+++ Assets/Scripts/InGame/Characters/CameraPivot.cs
@@ -0,0 +1,24 @@
+﻿using Sirenix.OdinInspector;
+using UnityEngine;
+
+namespace InGame.Characters
+{
+  [HideMonoScript]
+  public class CameraPivot : MonoBehaviour
+  {
+    [SerializeField]
+    private Transform transformToFollow;
+
+    private Transform _transform;
+
+    private void Awake() {
+      _transform = transform;
+    }
+
+    private void Update() {
+      var followedPosition = transformToFollow.position;
+      followedPosition.y = 0;
+      _transform.position = followedPosition;
+    }
+  }
+}
